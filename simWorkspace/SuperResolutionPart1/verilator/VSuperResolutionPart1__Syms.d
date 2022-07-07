917017e502bb391b6456d404c73f50f866216652
Charles <liqx36@mail2.sysu.edu.cn>
2022-07-08 01:13:35 +0800

    Delete simWorkspace directory

--- simWorkspace/SuperResolutionPart1/verilator/VSuperResolutionPart1__Syms.d
+++
@@ -1,7 +0,0 @@
-VSuperResolutionPart1__Syms.o: VSuperResolutionPart1__Syms.cpp \
- VSuperResolutionPart1__Syms.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated_types.h \
- /usr/local/share/verilator/include/verilated_funcs.h \
- VSuperResolutionPart1.h VSuperResolutionPart1___024root.h
--- simWorkspace/SuperResolutionPart1/verilator/VSuperResolutionPart1___024root__Slow.d
+++
@@ -1,8 +0,0 @@
-VSuperResolutionPart1___024root__Slow.o: \
- VSuperResolutionPart1___024root__Slow.cpp \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated_types.h \
- /usr/local/share/verilator/include/verilated_funcs.h \
- VSuperResolutionPart1__Syms.h VSuperResolutionPart1.h \
- VSuperResolutionPart1___024root.h
