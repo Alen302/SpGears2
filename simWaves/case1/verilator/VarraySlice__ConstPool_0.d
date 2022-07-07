d29dca6b5f33f36ddb716b9af1c42c577faa2398
Charles <liqx36@mail2.sysu.edu.cn>
2022-07-08 01:13:56 +0800

    Delete simWaves directory

--- simWaves/case1/verilator/VarraySlice__ConstPool_0.d
+++
@@ -1,5 +0,0 @@
-VarraySlice__ConstPool_0.o: VarraySlice__ConstPool_0.cpp \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated_types.h \
- /usr/local/share/verilator/include/verilated_funcs.h
--- simWaves/case1/verilator/VarraySlice__Syms__1.d
+++
@@ -1,7 +0,0 @@
-VarraySlice__Syms__1.o: VarraySlice__Syms__1.cpp VarraySlice__Syms.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated_types.h \
- /usr/local/share/verilator/include/verilated_funcs.h VarraySlice.h \
- /usr/local/share/verilator/include/vltstd/svdpi.h \
- VarraySlice___024root.h VarraySlice_arraySlice.h
